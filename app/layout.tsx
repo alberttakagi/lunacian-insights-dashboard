69c31279d76539f9b957fec3dce3cc48c25c312a
alberttakagi <albert@axieinfinity.com>
2026-01-29 17:50:56 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="bg-slate-950 text-slate-50 antialiased">{children}</body>
     </html>
   );
 }
