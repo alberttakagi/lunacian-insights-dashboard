09136860d3060f19fa609533e3cb2cf8f5dfa8ab
alberttakagi <albert@axieinfinity.com>
2026-01-29 17:31:42 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Lunacian Insights",
+  description: "Axie Infinity YouTube Analytics",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-slate-950 text-slate-50">{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
